4adeff939d2497c0dcb43c6e1f3ca922ec3d1d25
simamu530 <58772879+simamu530@users.noreply.github.com>
2021-01-23 17:34:56 +0900

    error

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,12 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div id="app"></div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  async created() {
+    await this.$store.dispatch("changeDataAction1");
+    await this.$store.dispatch("changeDataAction2")
+  },
+};
 </script>
